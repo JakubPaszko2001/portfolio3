05a2639be570a04b4f84dadd469bc4cf9aae5693
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-02-19 14:23:50 +0100

    zmiany w aos

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -26,7 +26,7 @@ const Skills = () => {
     { path: figma, name: "FIGMA" },
   ];
   return (
-    <section data-aos="fade-up" data-aos-offset="500" id="Skills">
+    <section data-aos="fade-up" id="Skills">
       <Title header={header} paragraph={paragraph} />
       <InfiniteScroll icons={iconsUpper} scrolling={"scrolling"} />
       <InfiniteScroll icons={iconsLower} scrolling={"scrolling2"} />
