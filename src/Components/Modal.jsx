32fa29bd2705df90bba1ee7aad0271f7b5146202
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-07 14:28:05 +0200

    fix: :art: change min-height

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { IoMdClose } from "react-icons/io";
 const Modal = ({ data }) => {
   return (
-    <dialog className="modal fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl w-full max-w-[600px] min-h-[200px] text-xl bg-light dark:bg-dark dark:text-light">
+    <dialog className="modal fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl w-full max-w-[600px] min-h-[250px] text-xl bg-light dark:bg-dark dark:text-light">
       <div className="flex flex-col gap-4 w-full h-full relative p-8">
         <p>{data?.information?.header}</p>
         <p>{data?.information?.paragraphs}</p>
